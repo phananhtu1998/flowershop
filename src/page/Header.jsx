7085dd9734aa80258303c47e96d1bb3005a6a108
phananhtu1998 <109066304+phananhtu1998@users.noreply.github.com>
2023-12-11 17:29:29 +0700

    commit code

--- src/page/Header.jsx
+++ src/page/Header.jsx
@@ -13,6 +13,7 @@ import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import "../style/global.css"
 const WebHeader = () => {
     //slider
     const images = [
